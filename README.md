74086cb64b0de91eaf939fcfc4b464ea79186ce6
ChrisMungiria <christopher.mungiria@strathmore.edu>
2023-05-24 21:05:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# world_time
+
+A new Flutter project.
+
+## Getting Started
+
+This project follows a YouTube tutorial from The Net Ninja - Flutter Tutorial for Beginners
+
+## Link
+
+The link -> https://www.youtube.com/watch?v=1ukSR1GRtMU&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
+
+// Start from lesson
+
+void main() => runApp(MaterialApp(
+  initialRoute: '/',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation()
+  }
+));
+
